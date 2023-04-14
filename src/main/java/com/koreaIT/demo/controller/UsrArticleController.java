9b2e09c3e71a3e83710e1a95e75343ff04752aad
choijangjun <jangjun9644@gmail.com>
2023-04-14 17:39:56 +0900

    writeArticle

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -18,18 +18,36 @@ public class UsrArticleController {
 	public UsrArticleController(){
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
-		
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
+	private Article writeArticle(String title, String body) {
 		int id = this.lastArticleId + 1;
 		this.lastArticleId = id;
 		
 		Article article = new Article(id, title, body);
 		
 		articles.add(article);
+		
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		
+		Article article = writeArticle(title, body);
 		
 		return article;
 	}
