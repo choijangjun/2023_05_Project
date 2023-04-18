9e6ce659a3eeefe123c37193aa9415e3b5d8082d
choijangjun <jangjun9644@gmail.com>
2023-04-18 15:30:51 +0900

    Util.f()

--- src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
@@ -45,13 +45,13 @@ public class UsrMemberController {
 		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
 		
 		if (id == -1) {
-			return "이미 사용중인 아이디입니다";
+			return Util.f("이미 사용중인 아이디(%s)입니다", loginId);
 		}
 		if (id == -2) {
-			return "이미 사용중인 닉네임입니다";
+			return Util.f("이미 사용중인 닉네임(%s)입니다", nickname);
 		}
 		if (id == -3) {
-			return "이미 사용중인 이름과 이메일입니다";
+			return Util.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email);
 		}
 		
 		return memberService.getMemberById(id);
