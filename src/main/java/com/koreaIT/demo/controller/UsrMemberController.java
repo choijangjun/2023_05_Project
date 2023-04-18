b6085e5a9506b36ea4ac690f08d70ab2e7140c48
choijangjun <jangjun9644@gmail.com>
2023-04-18 14:39:58 +0900

    Util.empty()

--- src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.koreaIT.demo.service.MemberService;
+import com.koreaIT.demo.util.Util;
 
 @Controller
 public class UsrMemberController {
@@ -22,22 +23,22 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 
-		if (loginId == null || loginId.trim().length() == 0) {
+		if (Util.empty(loginId)) {
 			return "아이디를 입력해주세요";
 		}
-		if (loginPw == null || loginPw.trim().length() == 0) {
+		if (Util.empty(loginPw)) {
 			return "비밀번호를 입력해주세요";
 		}
-		if (name == null || name.trim().length() == 0) {
+		if (Util.empty(name)) {
 			return "이름을 입력해주세요";
 		}
-		if (nickname == null || nickname.trim().length() == 0) {
+		if (Util.empty(nickname)) {
 			return "닉네임을 입력해주세요";
 		}
-		if (cellphoneNum == null || cellphoneNum.trim().length() == 0) {
+		if (Util.empty(cellphoneNum)) {
 			return "전화번호를 입력해주세요";
 		}
-		if (email == null || email.trim().length() == 0) {
+		if (Util.empty(email)) {
 			return "이메일을 입력해주세요";
 		}
 		
--- src/main/java/com/koreaIT/demo/util/Util.java
+++ src/main/java/com/koreaIT/demo/util/Util.java
@@ -1,5 +1,18 @@
 package com.koreaIT.demo.util;
 
 public class Util {
-
+	public static boolean empty(Object obj) {
+		
+		if (obj == null) {
+			return true;
+		}
+		
+//		if (obj instanceof String == false) {
+//			return true;
+//		}
+		
+		String str = (String) obj;
+		
+		return str.trim().length() == 0;
+	}
 }
