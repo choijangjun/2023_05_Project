04bb344bd2d489bf80982975c9f9ade340b80bc0
choijangjun <jangjun9644@gmail.com>
2023-04-19 15:01:33 +0900

    doAdd, doJoin에 ResultData 적용

--- src/main/java/com/koreaIT/demo/service/MemberService.java
+++ src/main/java/com/koreaIT/demo/service/MemberService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.koreaIT.demo.repository.MemberRepository;
+import com.koreaIT.demo.util.Util;
 import com.koreaIT.demo.vo.Member;
+import com.koreaIT.demo.vo.ResultData;
 
 @Service
 public class MemberService {
@@ -16,29 +18,29 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 	
-	public int doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 
 		Member existsMember = getMemberByLoginId(loginId);
 		
 		if (existsMember != null) {
-			return -1;
+			return ResultData.from("F-7", Util.f("이미 사용중인 아이디(%s) 입니다", loginId));
 		}
 		
 		existsMember = getMemberByNickname(nickname);
 		
 		if (existsMember != null) {
-			return -2;
+			return ResultData.from("F-8", Util.f("이미 사용중인 닉네임(%s) 입니다", nickname));
 		}
 		
 		existsMember = getMemberByNameAndEmail(name, email);
 		
 		if (existsMember != null) {
-			return -3;
+			return ResultData.from("F-9", Util.f("이미 사용중인 이름(%s)과 이메일(%s) 입니다", name, email));
 		}
 		
 		memberRepository.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
 		
-		return memberRepository.getLastInsertId();
+		return ResultData.from("S-1", Util.f("%s회원님이 가입되었습니다", loginId), memberRepository.getLastInsertId());
 	}
 	
 	private Member getMemberByNameAndEmail(String name, String email) {
