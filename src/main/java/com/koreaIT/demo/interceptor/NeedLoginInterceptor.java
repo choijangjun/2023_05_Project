6c2a962c2a1cbb073c1a023f5cac9135510c3464
choijangjun <jangjun9644@gmail.com>
2023-04-26 13:18:33 +0900

    jsPrintHistoryBack 수정

--- src/main/java/com/koreaIT/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/koreaIT/demo/interceptor/NeedLoginInterceptor.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.koreaIT.demo.vo.Rq;
+
 @Component
 public class NeedLoginInterceptor implements HandlerInterceptor {
 
@@ -13,6 +15,12 @@ public class NeedLoginInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
+		Rq rq = (Rq) request.getAttribute("Rq");
+		
+		if (rq.getLoginedMemberId() == 0) {
+			rq.jsPrintHistoryBack("로그인 후 이용해주세요");
+			return false;
+		}
 		
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
--- src/main/java/com/koreaIT/demo/vo/Rq.java
+++ src/main/java/com/koreaIT/demo/vo/Rq.java
@@ -36,14 +36,7 @@ public class Rq {
 	public void jsPrintHistoryBack(String msg) {
 		resp.setContentType("text/html; charset=UTF-8;");
 		
-		println("<script>");
-		
-		if (!Util.empty(msg)) {
-			println("alert('" + msg + "');");
-		}
-		
-		println("history.back();");
-		println("</script>");
+		print(Util.jsHistoryBack(msg));
 	}
 
 	private void print(String str) {
@@ -53,10 +46,5 @@ public class Rq {
 			e.printStackTrace();
 		}
 	}
-	
-	private void println(String str) {
-		print(str + "\n");
-	}
-	
 	
 }
