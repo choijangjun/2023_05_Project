e240568ce9955a287fce3d5b71665689b1fa9765
choijangjun <jangjun9644@gmail.com>
2023-04-28 15:02:46 +0900

    write에서 board선택

--- src/main/java/com/koreaIT/demo/service/ArticleService.java
+++ src/main/java/com/koreaIT/demo/service/ArticleService.java
@@ -20,8 +20,8 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 	
-	public void writeArticle(int memberId, String title, String body) {
-		articleRepository.writeArticle(memberId, title, body);
+	public void writeArticle(int memberId, int boardId, String title, String body) {
+		articleRepository.writeArticle(memberId, boardId, title, body);
 	}
 	
 	public int getLastInsertId() {
